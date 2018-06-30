84ddf46654cf371908ec3aa7d188e55008d8e7ef
Adam Marczyk <adam.marczyk90@gmail.com>
2018-06-30 22:45:25 +0200

    add compute bitcoin method

--- src/components/Loot.test.tsx
+++ src/components/Loot.test.tsx
@@ -5,7 +5,7 @@ import { Loot } from './Loot';
 
 describe('Loot', () => {
   const mockFetchBitcoin = jest.fn();
-  const props = { balance: 10, bitcoin: {}, fetchBitcoin: {} };
+  let props: any = { balance: 10, bitcoin: {}, fetchBitcoin: {} };
   let loot: any = shallow(<Loot {...props} />);
 
   it('renders properly', () => {
@@ -20,6 +20,17 @@ describe('Loot', () => {
 
     it('dispatches the `fetchBitcoin()` method it receives from props', () => {
       expect(mockFetchBitcoin).toHaveBeenCalled();
+    });
+  });
+
+  describe('when there are valid bitcoin props', () => {
+    beforeEach(() => {
+      props = { balance: 10, bitcoin: { bpi: { USD: { rate: '1,000' } } } };
+      loot = shallow(<Loot {...props} />);
+
+      it('displays the correct bitcoin value', () => {
+        expect(loot.find('h3').text()).toEqual('Bitcoin balance: 0.01');
+      });
     });
   });
 });
--- src/components/Loot.tsx
+++ src/components/Loot.tsx
@@ -4,14 +4,29 @@ import { fetchBitcoin } from '../actions/bitcoin';
 
 export interface ILootProps {
   fetchBitcoin: any;
+  bitcoin: any;
+  balance: number;
 }
 
 export class Loot extends React.Component<ILootProps, {}> {
   public componentDidMount() {
     this.props.fetchBitcoin();
   }
+
+  public computeBitcoin() {
+    const { bitcoin } = this.props;
+
+    if (Object.keys(bitcoin).length === 0) {
+      return '';
+    }
+
+    return (
+      this.props.balance / parseInt(bitcoin.bpi.USD.rate.replace(',', ''), 10)
+    );
+  }
+
   public render() {
-    return <h3>Bitcoin balance:</h3>;
+    return <h3>Bitcoin balance: {this.computeBitcoin()}</h3>;
   }
 }
 
