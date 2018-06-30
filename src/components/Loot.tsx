c687a0cd3d3671113d3fdc26457a0dfc47b7ed48
Adam Marczyk <adam.marczyk90@gmail.com>
2018-06-30 22:38:39 +0200

    fix

--- src/components/Loot.tsx
+++ src/components/Loot.tsx
@@ -2,13 +2,26 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { fetchBitcoin } from '../actions/bitcoin';
 
-export class Loot extends React.Component {
+export interface ILootProps {
+  fetchBitcoin: any;
+}
+
+export class Loot extends React.Component<ILootProps, {}> {
+  public componentDidMount() {
+    this.props.fetchBitcoin();
+  }
   public render() {
     return <h3>Bitcoin balance:</h3>;
   }
 }
 
+export const mapDispatchToProps = (dispatch: any) => {
+  return {
+    fetchBitcoin: () => dispatch(fetchBitcoin())
+  };
+};
+
 export default connect(
   state => state,
-  { fetchBitcoin }
+  mapDispatchToProps
 )(Loot);
