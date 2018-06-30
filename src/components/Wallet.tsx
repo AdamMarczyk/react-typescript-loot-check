429da3d8eb791fc3dcfb5aa3ee7617e636771ec6
Adam Marczyk <adam.marczyk90@gmail.com>
2018-06-30 17:10:57 +0200

    add wallet component

---
+++ src/components/Wallet.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Wallet extends React.Component {
+  public render() {
+    return (
+      <div>
+        <h3>Wallet Balance</h3>
+      </div>
+    );
+  }
+}
+
+export default Wallet;
