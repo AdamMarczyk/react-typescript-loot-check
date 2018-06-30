6236e2f295e4510a726158de6e386586e13aad9b
Adam Marczyk <adam.marczyk90@gmail.com>
2018-06-30 15:48:28 +0200

    add store interface

--- src/reducers/balance.tsx
+++ src/reducers/balance.tsx
@@ -1,12 +1,14 @@
 import { BalanceAction } from '../actions/balance';
 import * as constants from '../constants';
-const balance = (state: any = null, action: BalanceAction): any => {
+import { IStoreState } from '../types';
+const balance = (state: IStoreState, action: BalanceAction): any => {
   switch (action.type) {
     case constants.SET_BALANCE:
       return action.balance;
     default:
       return balance;
   }
+  return state;
 };
 
 export default balance;
