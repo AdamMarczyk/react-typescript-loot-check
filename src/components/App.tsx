e645adf0e1e024bba3acd5bb441451770ef27192
Adam Marczyk <adam.marczyk90@gmail.com>
2018-06-30 22:52:45 +0200

    convert sfc

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,18 +2,16 @@ import * as React from 'react';
 
 import Loot from './Loot';
 import Wallet from './Wallet';
-class App extends React.Component {
-  public render() {
-    return (
-      <div>
-        <h2>Loot Check</h2>
-        <hr />
-        <Wallet />
-        <hr />
-        <Loot />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h2>Loot Check</h2>
+      <hr />
+      <Wallet />
+      <hr />
+      <Loot />
+    </div>
+  );
+};
 
 export default App;
