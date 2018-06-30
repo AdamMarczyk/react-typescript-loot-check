3e26ee901d2222d31048c365fba6d009ae003655
Adam Marczyk <adam.marczyk90@gmail.com>
2018-06-30 20:01:05 +0200

    add cookie test later

--- src/reducers/balance.test.tsx
+++ src/reducers/balance.test.tsx
@@ -1,13 +1,23 @@
 import * as constants from '../constants';
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 
 describe('balanceReducer', () => {
-  it('sets a balance', () => {
+  describe('when initializing', () => {
     const balance: number = 10;
+    it('sets a balance', () => {
+      expect(
+        balanceReducer({ balance }, { type: constants.SET_BALANCE, balance })
+      ).toEqual(balance);
+    });
 
-    expect(
-      balanceReducer({ balance }, { type: constants.SET_BALANCE, balance })
-    ).toEqual(balance);
+    /* describe('then re-initializing', () => {
+      it('read the balance from cookies', () => {
+        expect(balanceReducer2(undefined, {})).toEqual({
+          balance
+        });
+      });
+    }); */
   });
 
   it('deposits into the balance', () => {
--- src/reducers/balance.tsx
+++ src/reducers/balance.tsx
@@ -1,21 +1,35 @@
+import { bake_cookie, read_cookie } from 'sfcookies';
+
 import { BalanceAction } from '../actions/balance';
 import * as constants from '../constants';
 import { IStoreState } from '../types';
+
+const BALANCE_COOKIE = 'BALANCE_COOKIE';
 
 const balance = (
   state: IStoreState = { balance: 0 },
   action: BalanceAction
 ): any => {
+  let balance;
+
   switch (action.type) {
     case constants.SET_BALANCE:
-      return action.balance;
+      balance = action.balance;
+      break;
     case constants.DEPOSIT:
-      return { ...state, balance: (state.balance += action.deposit) };
+      balance = { ...state, balance: (state.balance += action.deposit) };
+      break;
     case constants.WITHDRAW:
-      return { ...state, balance: (state.balance -= action.withdrawal) };
+      balance = { ...state, balance: (state.balance -= action.withdrawal) };
+      break;
     default:
-      return state;
+      balance = {
+        ...state,
+        balance: parseInt(read_cookie(BALANCE_COOKIE), 10 || state)
+      };
   }
+  bake_cookie(BALANCE_COOKIE, balance);
+  return balance;
 };
 
 export default balance;
