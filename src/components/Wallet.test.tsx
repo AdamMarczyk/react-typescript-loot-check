07397e62a8111abec8bdf1e3289fa1972972640c
Adam Marczyk <adam.marczyk90@gmail.com>
2018-06-30 18:53:04 +0200

    add update balance method and test

--- src/components/Wallet.test.tsx
+++ src/components/Wallet.test.tsx
@@ -1,11 +1,11 @@
 import { shallow } from 'enzyme';
 import * as React from 'react';
 
-import { Wallet } from './Wallet';
+import { IWalletState, Wallet } from './Wallet';
 
 describe('Wallet', () => {
   const props = { balance: 20 };
-  const wallet = shallow(<Wallet {...props} />);
+  const wallet = shallow<Wallet, {}, IWalletState>(<Wallet {...props} />);
 
   it('renders properly', () => {
     expect(wallet).toMatchSnapshot();
@@ -13,5 +13,23 @@ describe('Wallet', () => {
 
   it('displays the balance from props', () => {
     expect(wallet.find('.balance').text()).toEqual('Wallet Balance: 20');
+  });
+
+  it('creates an input to deposit into or withdraw from the balance', () => {
+    expect(wallet.find('.input-wallet').exists()).toBe(true);
+  });
+
+  describe('when the user types into the wallt input', () => {
+    const userBalance = '25';
+
+    beforeEach(() => {
+      wallet
+        .find('.input-wallet')
+        .simulate('change', { target: { value: userBalance } });
+    });
+
+    it('updates the local wallet balance in `state` and converts it to a number', () => {
+      expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
+    });
   });
 });
--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -6,11 +6,24 @@ export interface IWalletProps {
   balance: number;
 }
 
+export interface IWalletState {
+  balance: number;
+}
+
 export class Wallet extends React.Component<IWalletProps, {}> {
+  public state = {
+    balance: 0
+  };
+
+  public updateBalance = (event: any): void =>
+    this.setState({ balance: parseInt(event.target.value, 10) });
+
   public render() {
     return (
       <div>
         <h3 className="balance">Wallet Balance: {this.props.balance}</h3>
+        <br />
+        <input className="input-wallet" onChange={this.updateBalance} />
       </div>
     );
   }
