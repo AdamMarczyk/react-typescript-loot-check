ef8fa1250e856ad160ac5c7af824dc7565d0a68c
Adam Marczyk <adam.marczyk90@gmail.com>
2018-06-30 22:28:08 +0200

    add fetch test

--- src/components/Loot.test.tsx
+++ src/components/Loot.test.tsx
@@ -1,12 +1,25 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import * as React from 'react';
 
 import { Loot } from './Loot';
 
 describe('Loot', () => {
-  const loot = shallow(<Loot />);
+  const mockFetchBitcoin = jest.fn();
+  const props = { balance: 10, bitcoin: {}, fetchBitcoin: {} };
+  let loot: any = shallow(<Loot {...props} />);
 
-  it('renders properply', () => {
+  it('renders properly', () => {
     expect(loot).toMatchSnapshot();
+  });
+
+  describe('when mounted', () => {
+    beforeEach(() => {
+      props.fetchBitcoin = mockFetchBitcoin;
+      loot = mount(<Loot {...props} />);
+    });
+
+    it('dispatches the `fetchBitcoin()` method it receives from props', () => {
+      expect(mockFetchBitcoin).toHaveBeenCalled();
+    });
   });
 });
